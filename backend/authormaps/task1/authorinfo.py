f03e40dc7bcb0bd955b162377cb4fa5e0704097b
rathod <s0dhrath@uni-bonn.de>
2021-01-20 22:57:51 +0100

    "20-01-202/task1"

--- backend/authormaps/task1/authorinfo.py
+++ backend/authormaps/task1/authorinfo.py
@@ -11,30 +11,37 @@ PROJECT_DIR = os.path.join(home_dir, ".project", "group5")
 DATA_DIR = os.path.join(PROJECT_DIR,"data")
 
 
-class Author_data:
-    def __init__(self, author_name=None):
+class AuthorData:
+    def __init__(self, author_name=None,IDs_to_retrive=20):
         self.author = author_name
         self.AUTHOR_DIR = os.path.join(DATA_DIR, self.author)
         os.makedirs(self.AUTHOR_DIR, exist_ok=True)
+        self.retmax=IDs_to_retrive
 
     def get_list_of_publications(self) -> List['PMID']:
 
         type_ = "[Author - Full]"
         term = self.author + type_
-        handle = Entrez.esearch(db="pubmed", term=term)
+        handle = Entrez.esearch(db="pubmed", term=term,retmax=self.retmax)
         record = Entrez.read(handle)
         idlist = record['IdList']
-
+        print('Number of ids requested : {} '.format(self.retmax))
+        print('Number of ids available on NCBI : {}'.format(len(idlist)))
         return idlist
 
     def __cache_publications(self) -> None:
         ids = self.get_list_of_publications()
         for i in ids:
-            summ_handle_3 = Entrez.esummary(db="pubmed", id=i, version="2.0", retmode="json")
-            data = json.load(summ_handle_3)
-            path = os.path.join(self.AUTHOR_DIR, f"{i}.json")
-            with open(path, 'w') as outfile:
-                json.dump(data, outfile)
+            if str(i)+".json" in os.listdir(self.AUTHOR_DIR):
+#                 print("passing")
+                pass
+            else:
+#                 print("downloading")
+                summ_handle_3 = Entrez.esummary(db="pubmed", id=i, version="2.0", retmode="json")
+                data = json.load(summ_handle_3)
+                path = os.path.join(self.AUTHOR_DIR, f"{i}.json")
+                with open(path, 'w') as outfile:
+                    json.dump(data, outfile)
 
     def get_list_of_coauthors_from_list_of_publications(self, publications: List['PMID']) -> List[str]:
         self.__cache_publications()
@@ -54,6 +61,7 @@ class Author_data:
     def get_list_of_coauthors(self) -> List[str]:
         co_authors = self.get_list_of_coauthors_from_list_of_publications(self.get_list_of_publications())
         if co_authors == []:
+            os.rmdir(self.AUTHOR_DIR)
             return "No such author found"
         else:
             print(co_authors)
@@ -61,5 +69,9 @@ class Author_data:
 
 
 
-d=Author_data('William Joyce')
+d=AuthorData('Svjetlana Miocinovic',5)
 d.get_list_of_coauthors()
+
+#for author not found
+# d=AuthorData('xcfgsfg',50)
+# d.get_list_of_coauthors()
