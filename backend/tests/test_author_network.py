5cee77c7fa54b7400546b788dd7cb673f75cf473
Marlo <MarloB@online.de>
2021-01-30 00:03:02 +0100

    Ilyas suggested changes, but not testing for networkutils

--- backend/tests/test_author_network.py
+++ backend/tests/test_author_network.py
@@ -1,5 +1,5 @@
 import unittest
-from backend.authormaps import networkcreation
+from backend.authormaps import author_network
 import graphviz
 from os import path
 import os
@@ -8,20 +8,20 @@ test_data = {("Ilya", "Marlo"): 3, ("Pragya", "Dhruv"): 4, ("Marlo", "Dhruv"): 2
              ("Pragya", "Ilya"): 7}
 
 
-class Test_AuthorNetwork:
+class TestAuthorNetwork:
 
     def test_init(self):
-        test_graph = networkcreation.AuthorNetwork(test_data)
+        test_graph = author_network.AuthorNetwork(test_data)
         assert test_graph
         assert test_graph.data == test_data
         assert type(test_graph.graph) == graphviz.Graph
 
     def test_build_network(self):
-        tg = networkcreation.AuthorNetwork(test_data)
+        tg = author_network.AuthorNetwork(test_data)
         print(tg.graph.source)
         assert 'Ilya -- Marlo [label=3 color="0 0 0.4" penwidth=3.0]' in tg.graph.source
         # Non annotated test! Maybe prone to randomness?????????????
-        tgnoannotation = networkcreation.AuthorNetwork(test_data, enable_annotations=False)
+        tgnoannotation = author_network.AuthorNetwork(test_data, enable_annotations=False)
         nonannotext = """
 	Ilya -- Marlo
 	Pragya -- Dhruv
@@ -32,7 +32,7 @@ class Test_AuthorNetwork:
         assert nonannotext in tgnoannotation.graph.source
 
     def test_save_image(self):
-        tg = networkcreation.AuthorNetwork(test_data)
+        tg = author_network.AuthorNetwork(test_data)
         savepath = "Authorgraph.pdf"
         if path.exists(savepath):
             os.remove(savepath)
@@ -41,7 +41,8 @@ class Test_AuthorNetwork:
         tg.save_graph("pdf",view=False)
 
         assert path.exists(savepath)
-
+        if path.exists(savepath):
+            os.remove(savepath)
 
 if __name__ == '__main__':
     unittest.main()
