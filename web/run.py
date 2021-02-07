b4938eacc42242f1dc0a1c237d1da00d3344496a
mishra <s0prmish@uni-bonn.de>
2021-02-07 02:43:03 +0100

    ability to save image

--- web/run.py
+++ web/run.py
@@ -2,12 +2,8 @@ from flask import Flask, flash, request, redirect, url_for, render_template, ses
 from authormaps.authorinfo import AuthorData
 from authormaps.author_network import AuthorNetwork
 from authormaps.networkutils import count_shared_publications
-from pathlib import Path
 import os, io, base64
-
-home_dir = str(Path.home())
-PROJECT_DIR = os.path.join(home_dir, ".project", "group5")
-DATA_DIR = os.path.join(PROJECT_DIR, "data")
+import urllib
 
 app = Flask(__name__)
 app.secret_key = "someSecretKey"
@@ -27,28 +23,33 @@ def upload():
 @app.route('/info', methods=['GET', 'POST'])
 def display_data():
 
-    if request.method == 'POST':
-        seq = request.form.get("author")
+    try:
+        if request.method == 'POST':
+            seq = request.form.get("author")
 
-        if type(seq) == str:
-            session["author_name"] = seq
-            data = AuthorData(seq)
-            li = data.get_list_of_coauthors()
-
-            if type(li) == list:
+            if type(seq) == str:
+                session["author_name"] = seq
+                data = AuthorData(seq)
                 li = data.get_list_of_coauthors()
-                info = "The total number of Co-author's for {} is : {} . The co-author list is given below:".format(seq,len(li))
-                co_msg = "Would you like to see Co-author Network?"
-                return render_template('home.html', msg=info, res=sorted(li), next_msg=co_msg)
+
+                if type(li) == list:
+                    li = data.get_list_of_coauthors()
+                    info = "The total number of Co-author's for {} is : {} . The co-author list is given below:".format(seq,len(li))
+                    co_msg = "Would you like to see Co-author Network?"
+                    return render_template('home.html', msg=info, res=sorted(li), next_msg=co_msg)
+
+                else:
+                    return render_template('home.html',
+                                           msg="The author {} does not have any associated co-author's".format(seq), res="",
+                                           next_msg='')
 
             else:
-                return render_template('home.html',
-                                       msg="The author {} does not have any associated co-author's".format(seq), res="",
-                                       next_msg='')
+                return render_template('home.html', msg="Please enter it in the specified format", res="",
+                                       next_msg='')  # I don't understand why this isn't working
 
-        else:
-            return render_template('home.html', msg="Please enter it in the specified format", res="",
-                                   next_msg='')  # I don't understand why this isn't working
+    except urllib.error.URLError:
+        return render_template('home.html', msg="Please enter the name before clicking on Submit", res="",
+                               next_msg='')
 
 
 @app.route('/choose', methods=['GET', 'POST'])
@@ -65,10 +66,18 @@ def coauthor_map():
                 data = AuthorData(session["author_name"])
                 coauthors = data.get_list_of_coauthors()
                 shared_publications = count_shared_publications(coauthors)
+                new = []
+                for i, k in shared_publications.items():
+                    new.append({"a1": i[0], "a2": i[1], "count": k})
 
                 testobj = AuthorNetwork(shared_publications,highlight_authors=[selected[0], selected[1]])
 
                 if testobj is not None:
+
+                    session["author1"] = selected[0]
+                    session["author2"] = selected[1]
+                    session["shared_pub"] = new
+
                     graph = testobj.graph
                     chart_output = graph.pipe(format='png')
                     chart_output = base64.b64encode(chart_output).decode('utf-8')
@@ -91,22 +100,45 @@ def coauthor_map():
 @app.route('/saveoptions', methods=['GET', 'POST'])
 def save_rendered_img():
 
-    if request.method == 'POST':
-        option = request.form['radopt']
-        print(option)
+    try:
+        if request.method == 'POST':
+            option = request.form['radopt']
 
-        if option=='jpg':
-            print("selected option is jpg")
-            print(session["graph_obj"])
-#             session["graph_obj"].save_graph("jpg")
-        elif option=='pdf':
-            print("selected option is pdf")
-#             session["graph_obj"].save_graph("pdf")
-#         elif option=='svg':
-#             session["graph_obj"].save_graph("svg")
-#         elif option=='png':
-#             session["graph_obj"].save_graph("png")
-#         print("Saved")
+            print(option)
+            shared_publications = {}
+
+            for i in session["shared_pub"]:
+                j = list(i.values())
+                shared_publications[(j[0], j[1])] = j[2]
+
+            testobj = AuthorNetwork(shared_publications, highlight_authors=[session["author1"], session["author2"]])
+
+            if option=='jpg':
+                print("selected option is jpg")
+                testobj.save_graph("jpg")
+
+
+            elif option=='pdf':
+                print("selected option is pdf")
+                testobj.save_graph("pdf")
+    #
+            elif option=='svg':
+                print("selected option is svg")
+                testobj.save_graph("svg")
+
+            elif option=='png':
+                print("selected option is png")
+                testobj.save_graph("png")
+
+            else:
+                return render_template('plot.html', savemsg="Please select any file format before clicking on submit.")
+
+            print("Saved")
+            return render_template('plot.html', savemsg="File has been saved")
+
+    except KeyError:
+        return render_template('plot.html', savemsg="Please select any file format before clicking on submit.")
+
 
 
 
@@ -114,8 +146,8 @@ def save_rendered_img():
 
 
 if __name__ == '__main__':
-    Link = 'http://127.0.0.1:5000'
-    print(f"{Link} ")
+    # Link = 'http://127.0.0.1:5000'
+    # print(f"{Link} ")
     app.run(debug=True)
 
 # data = AuthorData('William Joyce')
